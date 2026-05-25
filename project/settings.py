3b1ad106443ec5894b1829bbf9c219dfc55250d2
Pratigya Adhikari <pratigya144@gmail.com>
2026-05-25 21:29:02 +0545

    api view

--- project/settings.py
+++ project/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rms",
+    
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- rms/views.py
+++ rms/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Category, Table
+from .serializer import CategorySerializer, TableSerializer
+
 
 # Create your views here.
+@api_view()
+def category(request):
+    category = Category.objects.all()
+    serializer = CategorySerializer(category, many=True)    #serialize: to convert queryset to json format
+    return Response(serializer.data)
+
+@api_view()
+def table(request):
+    table = Table.objects.all()
+    serializer = TableSerializer(table, many=True)    #serialize: to convert queryset to json format
+    return Response(serializer.data)
