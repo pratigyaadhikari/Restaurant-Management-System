899898a3c021b31306a56b8a854fec2eaf194bfd
Pratigya Adhikari <pratigya144@gmail.com>
2026-05-29 08:14:38 +0545

    delete operation

--- rms/urls.py
+++ rms/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import category,table, category_detail, table_detail
-
+# from .views import category,table, category_detail, table_detail
+from .views import CategoryAPIView, CategoryDetail, TableAPIView, TableDetail
 
 urlpatterns = [
-    path('category/', category),
-    path('category/<id>/', category_detail),
-    path('table/', table),
-    path('table/<id>/', table_detail)
+    # Class Based:
+    path('category/', CategoryAPIView.as_view()),
+    path('category/<id>/', CategoryDetail.as_view()),
+    path('table/', TableAPIView.as_view()),
+    path('table/<id>/',TableDetail.as_view())
+    
+    
+    
+    
+    # function based:
+    # path('category/', category),
+    # path('category/<id>/', category_detail),
+    # path('table/', table),
+    # path('table/<id>/', table_detail)
 ]
--- rms/views.py
+++ rms/views.py
@@ -6,60 +6,133 @@ from .serializer import CategorySerializer, TableSerializer
 
 
 # Create your views here.
-@api_view(['GET','POST'])
-def category(request):
-    if request.method == 'GET':
+
+# Class Based: APIView
+from rest_framework.views import APIView
+
+class CategoryAPIView(APIView):
+    def get(self, request):
         category = Category.objects.all()
-        serializer = CategorySerializer(category, many=True)    #serialize: to convert queryset to json format
+        serializer = CategorySerializer(category, many = True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+    
+    def post(self, request):
         serializer = CategorySerializer(data = request.data)    #deserialize: json format convert to model instance
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    
-@api_view(['GET','POST','DELETE'])
-def category_detail(request, id):
-    category = Category.objects.get(id = id)
-    if request.method == 'GET':
+
+
+class CategoryDetail(APIView):
+    def get(self, request, id):
+        category = Category.objects.get(id = id)
         serializer = CategorySerializer(category)
         return Response(serializer.data)
-    elif request.method == 'POST':
+    
+    def post(self, request, id):
+        category = Category.objects.get(id = id)
         serializer = CategorySerializer(category, data = request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+    
+    def delete(self, request, id):
+        category =  Category.objects.get(id = id)
         items = OrderItem.objects.filter(food__category = category).count()
         if items > 0:
-            return Response({"detail":"category cannoy be deleted. Protected in OrderItem"})
+            return Response({"details":"category cannot be deleted. Protected in OrderItem"})
         category.delete()
-        return Response({"detail": "Category deleted successfully"})
+        return Response({"details":"Category deleted successfully"})
+    
+        
+# Function-based: api_view()
+
+# @api_view(['GET','POST'])
+# def category(request):
+#     if request.method == 'GET':
+#         category = Category.objects.all()
+#         serializer = CategorySerializer(category, many=True)    #serialize: to convert queryset to json format
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = CategorySerializer(data = request.data)    #deserialize: json format convert to model instance
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+    
+# @api_view(['GET','POST','DELETE'])
+# def category_detail(request, id):
+#     category = Category.objects.get(id = id)
+#     if request.method == 'GET':
+#         serializer = CategorySerializer(category)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = CategorySerializer(category, data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#     elif request.method == 'DELETE':
+#         items = OrderItem.objects.filter(food__category = category).count()
+#         if items > 0:
+#             return Response({"detail":"category cannoy be deleted. Protected in OrderItem"})
+#         category.delete()
+#         return Response({"detail": "Category deleted successfully"})
     
     
-@api_view(['GET','POST'])
-def table(request):
-    if request.method == 'GET':
+# @api_view(['GET','POST'])
+# def table(request):
+#     if request.method == 'GET':
+#         table = Table.objects.all()
+#         serializer = TableSerializer(table, many=True)    #serialize: to convert queryset to json format
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = TableSerializer(data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+    
+# @api_view(['GET','POST'])
+# def table_detail(request, id):
+#     table = Table.objects.get(id = id)
+#     if request.method == 'GET':
+#         serializer = TableSerializer(table)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = TableSerializer(table, data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+    
+        
+
+class TableAPIView(APIView):
+    def get(self, request):
         table = Table.objects.all()
-        serializer = TableSerializer(table, many=True)    #serialize: to convert queryset to json format
+        serializer = TableSerializer(table, many= True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+    
+    def post(self, request):
         serializer = TableSerializer(data = request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
     
-@api_view(['GET','POST'])
-def table_detail(request, id):
-    table = Table.objects.get(id = id)
-    if request.method == 'GET':
+
+class TableDetail(APIView):
+    def get(self, request, id):
+        table = Table.objects.get(id = id)
         serializer = TableSerializer(table)
         return Response(serializer.data)
-    elif request.method == 'POST':
+        
+    def push(self, request, id):
+        table = Table.objects.get(id = id)
         serializer = TableSerializer(table, data = request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
     
+    def delete(self, request, id):
+        table = Table.objects.get(id = id)
+        table.delete()
+        return Response({"detail": "table deleted successfully"})
+            
         
-
