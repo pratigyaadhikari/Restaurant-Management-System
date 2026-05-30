2e1a0dea6dc3d78809a50c0d53f18f7c1280036a
Pratigya Adhikari <pratigya144@gmail.com>
2026-05-30 21:27:30 +0545

    genericAPI mixins

--- rms/urls.py
+++ rms/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 # from .views import category,table, category_detail, table_detail
-from .views import CategoryAPIView, CategoryDetail, TableAPIView, TableDetail
-
+# from .views import CategoryAPIView, CategoryDetail, TableAPIView, TableDetail
+from .views import CategoryGenericAPIView, CategoryDetail
 urlpatterns = [
+    # Mixins:
+    path("category/", CategoryGenericAPIView.as_view()),
+    path("category/<id>/", CategoryDetail.as_view())    #esma ni id ko satta pk rakhni
+    ]
+    
+    
     # Class Based:
-    path('category/', CategoryAPIView.as_view()),
-    path('category/<id>/', CategoryDetail.as_view()),
-    path('table/', TableAPIView.as_view()),
-    path('table/<id>/',TableDetail.as_view())
+    # path('category/', CategoryAPIView.as_view()),
+    # path('category/<id>/', CategoryDetail.as_view()),
+    # path('table/', TableAPIView.as_view()),
+    # path('table/<id>/',TableDetail.as_view())
     
     
     
@@ -17,4 +23,4 @@ urlpatterns = [
     # path('category/<id>/', category_detail),
     # path('table/', table),
     # path('table/<id>/', table_detail)
-]
+
--- rms/views.py
+++ rms/views.py
@@ -6,44 +6,105 @@ from .serializer import CategorySerializer, TableSerializer
 
 
 # Create your views here.
+# Mixins:
+from rest_framework import mixins, generics
+
+class CategoryGenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+    def get(self,request):
+        return self.list(request)       #if useing mixin yo line matra lekhda vo insted of mathi ko 3 ota line code
+        
+        # category = Category.objects.all()
+        # serializer = CategorySerializer(category, many = True)
+        # return Response(serializer.data)
+        
+    def post(self, request):
+        return self.create(request)
+    
+    
+    
+class CategoryDetail(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'id'
+    # (if lookup field use gardina vaneni , we can use pk)
+    def get(self,request,id):       #id = pk rakhni
+        return self.retrieve(request, id)
+    
+    
+   
+
+
+
+
 
 # Class Based: APIView
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 
-class CategoryAPIView(APIView):
-    def get(self, request):
-        category = Category.objects.all()
-        serializer = CategorySerializer(category, many = True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = CategorySerializer(data = request.data)    #deserialize: json format convert to model instance
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+# class CategoryAPIView(APIView):
+#     def get(self, request):
+#         category = Category.objects.all()
+#         serializer = CategorySerializer(category, many = True)
+#         return Response(serializer.data)
+#     def post(self, request):
+#         serializer = CategorySerializer(data = request.data)    #deserialize: json format convert to model instance
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
 
 
-class CategoryDetail(APIView):
-    def get(self, request, id):
-        category = Category.objects.get(id = id)
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
+# class CategoryDetail(APIView):
+#     def get(self, request, id):
+#         category = Category.objects.get(id = id)
+#         serializer = CategorySerializer(category)
+#         return Response(serializer.data) 
+#     def post(self, request, id):
+#         category = Category.objects.get(id = id)
+#         serializer = CategorySerializer(category, data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#     def delete(self, request, id):
+#         category =  Category.objects.get(id = id)
+#         items = OrderItem.objects.filter(food__category = category).count()
+#         if items > 0:
+#             return Response({"details":"category cannot be deleted. Protected in OrderItem"})
+#         category.delete()
+#         return Response({"details":"Category deleted successfully"})
     
-    def post(self, request, id):
-        category = Category.objects.get(id = id)
-        serializer = CategorySerializer(category, data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+     
+# class TableAPIView(APIView):
+#     def get(self, request):
+#         table = Table.objects.all()
+#         serializer = TableSerializer(table, many= True)
+#         return Response(serializer.data)
     
-    def delete(self, request, id):
-        category =  Category.objects.get(id = id)
-        items = OrderItem.objects.filter(food__category = category).count()
-        if items > 0:
-            return Response({"details":"category cannot be deleted. Protected in OrderItem"})
-        category.delete()
-        return Response({"details":"Category deleted successfully"})
+#     def post(self, request):
+#         serializer = TableSerializer(data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
     
+
+# class TableDetail(APIView):
+#     def get(self, request, id):
+#         table = Table.objects.get(id = id)
+#         serializer = TableSerializer(table)
+#         return Response(serializer.data)
+        
+#     def push(self, request, id):
+#         table = Table.objects.get(id = id)
+#         serializer = TableSerializer(table, data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+    
+#     def delete(self, request, id):
+#         table = Table.objects.get(id = id)
+#         table.delete()
+#         return Response({"detail": "table deleted successfully"})
         
 # Function-based: api_view()
 
@@ -104,35 +165,5 @@ class CategoryDetail(APIView):
     
         
 
-class TableAPIView(APIView):
-    def get(self, request):
-        table = Table.objects.all()
-        serializer = TableSerializer(table, many= True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = TableSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
 
-class TableDetail(APIView):
-    def get(self, request, id):
-        table = Table.objects.get(id = id)
-        serializer = TableSerializer(table)
-        return Response(serializer.data)
-        
-    def push(self, request, id):
-        table = Table.objects.get(id = id)
-        serializer = TableSerializer(table, data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
-    def delete(self, request, id):
-        table = Table.objects.get(id = id)
-        table.delete()
-        return Response({"detail": "table deleted successfully"})
-            
         
